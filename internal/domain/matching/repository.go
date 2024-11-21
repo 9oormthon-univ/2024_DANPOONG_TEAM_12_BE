52ecc6b3dbd6d8b50fc6434df7ee2fdb9be5bbc0
Jihyeon An <jh011007@naver.com>
2024-11-21 11:07:14 +0900

    [feat] add topLikeMatchingPosts

--- internal/domain/matching/repository.go
+++ internal/domain/matching/repository.go
@@ -1,6 +1,10 @@
 package matching
 
-import "gorm.io/gorm"
+import (
+	"github.com/9oormthon-univ/2024_DANPOONG_TEAM_12_BE/internal/types"
+	"gorm.io/gorm"
+	"log"
+)
 
 type MatchingRepository struct {
 	DB *gorm.DB
@@ -11,4 +15,27 @@ func SetMatchingRepository(DB *gorm.DB) *MatchingRepository {
 		DB: DB,
 	}
 	return r
+}
+
+func (r *MatchingRepository) GetTopLikedMatchingPosts(limit int) ([]types.MatchingTopLikesResponseDTO, error) {
+	var posts []types.MatchingTopLikesResponseDTO
+	result := r.DB.Table("matching").
+		Select("matching_id, title, image_url, details, likes AS likes_count").
+		Where("status = ?", "active").
+		Order("likes DESC").
+		Limit(limit).
+		Scan(&posts)
+
+	// 디버깅 로그 추가
+	if result.Error != nil {
+		log.Printf("Error fetching posts: %v", result.Error)
+		return nil, result.Error
+	}
+
+	// 데이터가 없을 때
+	if len(posts) == 0 {
+		log.Println("No matching posts found")
+		return nil, result.Error
+	}
+	return posts, nil
 }
